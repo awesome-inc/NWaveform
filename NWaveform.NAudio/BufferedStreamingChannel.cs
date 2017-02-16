44e6635ca5f971730836d8e9c18cdadcbf9c4070
mkoertgen <marcel.koertgen@gmail.com>
2017-02-16 12:25:11 +0100

    Added wrap around tests

--- NWaveform.NAudio/BufferedStreamingChannel.cs
+++ NWaveform.NAudio/BufferedStreamingChannel.cs
@@ -7,18 +7,24 @@ namespace NWaveform.NAudio
     {
         private readonly WaveProviderEx _waveProvider;
         internal readonly BufferedWaveStream BufferedStream;
+        private TimeSpan _preserveAfterWrapAround;
+        public TimeSpan BufferSize => BufferedStream.BufferDuration;
 
         public string Name { get; }
         public IWaveProviderEx Stream => _waveProvider;
 
-        public TimeSpan PreserveAfterWrapAround { get; set; }
-        public TimeSpan WarnBeforeWrapAround { get; set; }
+        public TimeSpan PreserveAfterWrapAround
+        {
+            get { return _preserveAfterWrapAround; }
+            set
+            {
+                if (value >= BufferSize) throw new ArgumentOutOfRangeException(nameof(value), $"{nameof(PreserveAfterWrapAround)} must be less than {nameof(BufferSize)}");
+                _preserveAfterWrapAround = value;
+            }
+        }
 
         public BufferedStreamingChannel(string name, WaveFormat waveFormat, TimeSpan bufferSize)
         {
-            PreserveAfterWrapAround = TimeSpan.FromSeconds(bufferSize.TotalSeconds / 6);
-            WarnBeforeWrapAround = TimeSpan.FromSeconds(30);
-
             Name = name;
             BufferedStream = new BufferedWaveStream(waveFormat, bufferSize)
             {
@@ -26,7 +32,6 @@ namespace NWaveform.NAudio
                 ReadFully = true,
             };
             _waveProvider = new WaveProviderEx(BufferedStream) { Closeable = false };
-
         }
 
         protected internal void AddSamples(TimeSpan time, byte[] buffer, int numBytes = -1)
--- NWaveform.NAudio/BufferedStreamingChannel_Should.cs
+++ NWaveform.NAudio/BufferedStreamingChannel_Should.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using FluentAssertions;
 using NAudio.Wave;
 using NEdifis.Attributes;
@@ -31,14 +32,39 @@ namespace NWaveform.NAudio
                 var actual = new byte[expected.Length];
                 var numbytes = stream.Read(actual, 0, actual.Length);
                 numbytes.Should().Be(expected.Length);
-                actual.ShouldAllBeEquivalentTo(expected);
+                actual.Should().Equal(expected);
             }
         }
 
         [Test]
-        public void Preserve_after_wrap_around()
+        [TestCase(0.5)]
+        public void Preserve_after_wrap_around(double preserveFactor)
         {
-            Assert.Fail("TODO");
+            var waveFormat = WaveFormat.CreateIeeeFloatWaveFormat(8000, 1);
+            var bufferSize = TimeSpan.FromSeconds(2);
+            using (var sut = new BufferedStreamingChannel("name", waveFormat, bufferSize))
+            {
+                sut.PreserveAfterWrapAround.Should().Be(TimeSpan.Zero, "default preserve should be zero (no preservation)");
+
+                sut.Invoking(x => x.PreserveAfterWrapAround = bufferSize)
+                    .ShouldThrow<ArgumentOutOfRangeException>("preserve must be less than buffer size");
+
+                sut.PreserveAfterWrapAround = TimeSpan.FromSeconds(bufferSize.TotalSeconds * preserveFactor);
+
+                var data = waveFormat.Generate(bufferSize);
+
+                sut.AddSamples(TimeSpan.Zero, data);
+
+                var preserved = (int)(data.Length * sut.PreserveAfterWrapAround.TotalSeconds / sut.BufferSize.TotalSeconds);
+                var skipped = data.Length - preserved;
+                var expected = data.Skip(skipped).Take(preserved).Concat(Enumerable.Repeat((byte) 0, skipped)).ToArray();
+
+                var stream = sut.BufferedStream;
+                var actual = new byte[expected.Length];
+                var numbytes = stream.Read(actual, 0, actual.Length);
+                numbytes.Should().Be(expected.Length);
+                actual.Should().Equal(expected);
+            }
         }
 
         [Test]
