e60ee0463ee4c93f0006bc72a7f9397afd221f8e
Thomas Mentzel <>
2017-12-12 13:30:25 +0100

    (cleanup) build bat updated.

--- build.bat
+++ build.bat
@@ -5,6 +5,18 @@ if exist "%msbuild%" goto checkNuget
 set msbuild=
 for %%a in (MSBuild.exe) do (set msbuild=%%~$PATH:a)
 if exist %msbuild% goto checkNuget
+
+rem Visual Studio Build Tools 2017
+set msbuild=C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe
+if exist "%msbuild%" goto checkNuget
+rem VS2017
+set msbuild=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe
+if exist "%msbuild%" goto checkNuget
+set msbuild=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe
+if exist "%msbuild%" goto checkNuget
+set msbuild=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe
+if exist "%msbuild%" goto checkNuget
+
 rem VS2015, 2013, cf.: http://blogs.msdn.com/b/visualstudio/archive/2013/07/24/msbuild-is-now-part-of-visual-studio.aspx
 set msbuild=c:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe
 if exist "%msbuild%" goto checkNuget
@@ -19,6 +31,9 @@ echo error: can't find MSBuild.exe. Is .NET Framework installed?
 exit /B 2
 
 :checkNuget
+echo ==========================================
+echo %msbuild%
+echo ==========================================
 for %%a in (NuGet.exe) do (set nugetPath=%%~$PATH:a)
 if not defined nugetPath (
 	echo error: can't find NuGet.exe in your path.
